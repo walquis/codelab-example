c2f0bc48ffeb66ef621a595d3171c4e1c2e2da4c
Chris Walquist <cwalquist@drw.com>
2022-11-28 21:19:56 -0600

    -s option skips docker image tag-build-push

--- bin/docker-build-k8s-deploy.sh
+++ bin/docker-build-k8s-deploy.sh
@@ -1,18 +1,31 @@
-image_tag=$(date +%Y-%m-%d_%H-%M-%S)
-
-echo $image_tag > docker_image_version.txt
-git add docker_image_version.txt
-git commit -m "Bumped Harbor-published image version to $image_tag"
-git push
-
-image_tag=`cat docker_image_version.txt`
-
-docker build -t prod.harbor.drw/apprenticeship/codelabs:$image_tag .
-
-docker push  prod.harbor.drw/apprenticeship/codelabs:$image_tag
+skip_push=
+while getopts 's' opt; do
+  case "$opt" in
+    s)
+      echo "Skipping Docker tag-create-build-push..."
+      skip_push=yes
+      ;;
+  esac
+done
 
 harbor_url=prod.harbor.drw/apprenticeship/codelabs
 
-sed -i '' "s#${harbor_url}.*#${harbor_url}:$image_tag#" deploy/deployment.yml
+if [[ "$skip_push" != "yes" ]]; then
+  image_tag=$(date +%Y-%m-%d_%H-%M-%S)
+  echo $image_tag > docker_image_version.txt
+
+  sed -i '' "s#${harbor_url}.*#${harbor_url}:$image_tag#" deploy/deployment.yml
+
+  git add docker_image_version.txt deploy/deployment.yml
+  git commit -m "Bumped Harbor-published image version to $image_tag"
+  git push
+
+  docker build -t prod.harbor.drw/apprenticeship/codelabs:$image_tag .
+
+  docker push prod.harbor.drw/apprenticeship/codelabs:$image_tag
+else
+  image_tag=`cat docker_image_version.txt`
+  sed -i '' "s#${harbor_url}.*#${harbor_url}:$image_tag#" deploy/deployment.yml
+fi
 
 kubectl apply -f deploy
