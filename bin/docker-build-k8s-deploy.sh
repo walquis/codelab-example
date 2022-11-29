65bb1c4fa1effa6142f4589d0ccd9c0311e7e48b
Chris Walquist <cwalquist@drw.com>
2022-11-28 20:49:00 -0600

    Add version-controlled  image version file to the docker build script

--- bin/docker-build-k8s-deploy.sh
+++ bin/docker-build-k8s-deploy.sh
@@ -1,5 +1,12 @@
 image_tag=$(date +%Y-%m-%d_%H-%M-%S)
 
+echo $image_tag > docker_image_version.txt
+git add docker_image_version.txt
+git commit -m "Bumped Harbor-published image version to $image_tag"
+git push
+
+image_tag=`cat docker_image_version.txt`
+
 docker build -t prod.harbor.drw/apprenticeship/codelabs:$image_tag .
 
 docker push  prod.harbor.drw/apprenticeship/codelabs:$image_tag
